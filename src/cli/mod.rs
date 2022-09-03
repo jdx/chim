54a8c591f84f462b9c800e8cfee04664a2791f01
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2022-09-03 15:26:19 -0500

    clippy

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -20,7 +20,7 @@ pub async fn parse(args: Vec<String>) -> Result<()> {
     match args.get(1) {
         Some(arg) => match arg.as_ref() {
             "-v" | "--version" | "version" => version::run(),
-            arg if arg.starts_with(".") || arg.starts_with("/") => app::run(args).await,
+            arg if arg.starts_with('.') || arg.starts_with('/') => app::run(args).await,
             _ => usage::run(),
         },
         None => usage::run(),
