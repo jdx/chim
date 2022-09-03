a49dfeea2fe442596985fdb2975aa5e0654dacb7
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2022-09-03 14:43:31 -0500

    win fix

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -1,5 +1,5 @@
 Get-ChildItem target\release
 New-Item dist\chim\bin -ItemType Directory -ea 0
-Move-Item target\release\chim.exe dist\chim\bin\chim.exe
+Copy-Item target\release\chim.exe dist\chim\bin\chim.exe
 $Env:CHIM_VERSION = (cargo get version --pretty)
 Compress-Archive -Path dist\chim -DestinationPath dist\chim-$env:CHIM_VERSION-windows-x64.zip
