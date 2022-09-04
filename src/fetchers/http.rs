aef687537b31266bd3669a92a0c3c0f0d5473cce
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2022-09-04 15:40:40 -0500

    error out on invalid HTTP status

--- src/fetchers/http.rs
+++ src/fetchers/http.rs
@@ -5,6 +5,7 @@ use std::path::Path;
 
 pub async fn fetch(url: &str, output: &Path) -> Result<()> {
     let response = reqwest::get(url).await?;
+    response.error_for_status_ref()?;
     let mut file = File::create(output)?;
     let mut content = Cursor::new(response.bytes().await?);
     std::io::copy(&mut content, &mut file)?;
